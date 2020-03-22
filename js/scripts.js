58cdf8192a872b631876039fb0672100ea580cfe
Gauss2 <luanprojetosidk@gmail.com>
2020-03-22 19:28:23 -0300

    first commit: adding files.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function ToggleMenu()
+{
+    for (let x of document.getElementsByTagName('li'))
+    {
+        if (!x.style.display || x.style.display == 'none')
+            x.style.display = 'block';
+        else
+        x.style.display = 'none';
+    }
+}
